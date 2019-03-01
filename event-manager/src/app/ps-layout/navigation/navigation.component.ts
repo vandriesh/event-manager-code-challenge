3302aef2a5c858eec5ff4a93a48f770428068374
Vitalie Andries <vitalie.andries@gmail.com>
2019-03-01 17:05:40 +0200

    adding prettier

--- event-manager/src/app/ps-layout/navigation/navigation.component.ts
+++ event-manager/src/app/ps-layout/navigation/navigation.component.ts
@@ -9,13 +9,9 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map((result) => result.matches));
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
-
-  constructor(private breakpointObserver: BreakpointObserver) {
-  }
-
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
